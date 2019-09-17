1128b166a2c06d7bd6ae598b0ea7aee93cec8597
Matt <matias.mbz@gmail.com>
2019-09-17 18:56:59 -0300

    FrontEnd W3C

---
+++ W3C/W3School/W3CSS/W3CSS-colors/w3-color-themes/script.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# curl -o w3-theme-amber.css  https://www.w3schools.com/lib/w3-theme-amber.css
+# curl -o w3-theme-black.css	https://www.w3schools.com/lib/w3-theme-black.css
+# curl -o w3-theme-blue.css	https://www.w3schools.com/lib/w3-theme-blue.css
+# curl -o w3-theme-blue-grey.css	https://www.w3schools.com/lib/w3-theme-blue-grey.css
+# curl -o w3-theme-brown.css	https://www.w3schools.com/lib/w3-theme-brown.css
+# curl -o w3-theme-cyan.css	https://www.w3schools.com/lib/w3-theme-cyan.css
+# curl -o w3-theme-dark-grey.css	 https://www.w3schools.com/lib/w3-theme-dark-grey.css
+# curl -o w3-theme-deep-orange.css	 https://www.w3schools.com/lib/w3-theme-deep-orange.css
+# curl -o w3-theme-deep-purple.css	 https://www.w3schools.com/lib/w3-theme-deep-purple.css
+# curl -o w3-theme-green.css	https://www.w3schools.com/lib/w3-theme-green.css
+# curl -o w3-theme-grey.css	https://www.w3schools.com/lib/w3-theme-grey.css
+# curl -o w3-theme-indigo.css https://www.w3schools.com/lib/w3-theme-indigo.css
+# curl -o w3-theme-khaki.css https://www.w3schools.com/lib/w3-theme-khaki.css
+# curl -o w3-theme-light-blue.css	 https://www.w3schools.com/lib/w3-theme-light-blue.css
+# curl -o w3-theme-light-green.css https://www.w3schools.com/lib/w3-theme-light-green.css
+# curl -o w3-theme-lime.css	 https://www.w3schools.com/lib/w3-theme-lime.css
+# curl -o w3-theme-orange.css	 https://www.w3schools.com/lib/w3-theme-orange.css
+# curl -o w3-theme-pink.css	https://www.w3schools.com/lib/w3-theme-pink.css
+# curl -o w3-theme-purple.css https://www.w3schools.com/lib/w3-theme-purple.css
+# curl -o w3-theme-red.css https://www.w3schools.com/lib/w3-theme-red.css
+# curl -o w3-theme-teal.css	https://www.w3schools.com/lib/w3-theme-teal.css
+# curl -o w3-theme-yellow.css https://www.w3schools.com/lib/w3-theme-yellow.css
+
+# chmod u+x script.sh
+# ./script.sh  o  bash script.sh
+# 
